8fa57f0485e74e177433183e6faa1dbf1bec4f4b
舒全刚 <645064175@qq.com>
2018-04-08 19:11:19 +0800

    跟新

--- app/Service/Admin/UserService.php
+++ app/Service/Admin/UserService.php
@@ -9,12 +9,35 @@
 namespace App\Service\Admin;
 
 
+use App\Model\User\Users;
 use App\Service\AdminBase;
+use Illuminate\Support\Facades\Cache;
+
 
 class UserService extends AdminBase
 {
-   public function getA()
+
+    /**
+     * @param $request
+     * @return mixed
+     * 经纪人列表
+     */
+   public function getList( $request )
    {
-       return 1666;
+       $tag = 'brokerList';
+       $where = $request->input('page').$request->input('companyid').$request->input('economictid');
+       $value = Cache::tags($tag)->remember( $tag.$where,config('configure.sCache'), function() use( $request ){
+           $sql = Users::orderBy('id','desc');
+           if( $request->input('companyid') )
+           {
+               $sql->where('companyid',$request->input('companyid'));
+           }
+           if( $request->input('economictid') )
+           {
+               $sql->where('economictid',$request->input('economictid'));
+           }
+           return $sql->paginate(config('configure.sPage'));
+       });
+       return $value;
    }
 }
