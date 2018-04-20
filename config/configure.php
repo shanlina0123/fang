8f4591b53c6949cecf97061fdec98bdba7a52faf
单莉娜 <1161110682@qq.com>
2018-04-20 11:49:18 +0800

    update

--- config/configure.php
+++ config/configure.php
@@ -21,73 +21,47 @@ return [
         //local - 单莉娜 - 域名
         'http://local.fang.com', //接口
         'http://local.renrenmobile.com',//手机
-        //local- 单莉娜 - IP
-        'http://192.168.15.28:8081', //接口
-        "http://192.168.15.28:8082",//手机
-
-        //local - 舒全刚- 域名
-        'http://local.fang.com', //接口
-        'http://local.renrenmobile.com',//手机
-        //local - 舒全刚- IP
-        'http://192.168.15.30:8081', //接口
-        "http://192.168.15.30:8082",//手机
-
-        //local - 赵颖- 域名
-        'http://local.renrenmobile.com',//手机
-        //local - 赵颖- IP
-        "http://192.168.15.37:8082",//手机
-
-        //local - 王彤- 域名
-        'http://local.renrenmobile.com',//手机
-        //local - 王彤- IP
-        "http://192.168.15.32:8082",//手机
-
-        //dev - 域名
-        'http://dev.renren.com',//接口
-        'http://dev.renremobile.com',//手机
-        //dev - IP
-        "http://192.168.15.13:8081",//接口
-        "http://192.168.15.13:8082",//手机
-
-
-        //生产 - 域名
-        'http://api.rrzhaofang.com',//接口
-        'http://wx.rrzhaofang.com',//手机
-
-        //local - 单莉娜 - 域名
-        'http://local.fang.com', //接口
         'http://local.renrenpc.com',//PC
         //local- 单莉娜 - IP
         'http://192.168.15.28:8081', //接口
+        "http://192.168.15.28:8082",//手机
         "http://192.168.15.28:8083",//PC
 
         //local - 舒全刚- 域名
         'http://local.fang.com', //接口
+        'http://local.renrenmobile.com',//手机
         'http://admin.renrenpc.com',//PC
         //local - 舒全刚- IP
         'http://192.168.15.30:8081', //接口
+        "http://192.168.15.30:8082",//手机
         "http://192.168.15.30:8083",//PC
 
         //local - 赵颖- 域名
+        'http://local.renrenmobile.com',//手机
         'http://local.renrenpc.com',//PC
         //local - 赵颖- IP
+        "http://192.168.15.37:8082",//手机
         "http://192.168.15.37:8083",//PC
 
         //local - 王彤- 域名
+        'http://local.renrenmobile.com',//手机
         'http://local.renrenpc.com',//PC
         //local - 王彤- IP
+        "http://192.168.15.32:8082",//手机
         "http://192.168.15.32:8083",//PC
 
         //dev - 域名
         'http://dev.renren.com',//接口
+        'http://dev.renremobile.com',//手机
         'http://dev.renrenpc.com',//PC
         //dev - IP
         "http://192.168.15.13:8081",//接口
+        "http://192.168.15.13:8082",//手机
         "http://192.168.15.13:8083",//PC
-
 
         //生产 - 域名
         'http://api.rrzhaofang.com',//接口
+        'http://wx.rrzhaofang.com',//手机
         'http://admin.rrzhaofang.com',//PC
     ],
 
